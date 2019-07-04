337646f5f2c2cfb7310efdb53cfca5306133ddae
Bilger Yahov <byahov@SOF-MBP-026.local>
2019-07-04 10:11:09 +0300

    Make sure that new to-do items can be added.

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -6,6 +6,28 @@ function Todo({ todo, index }) {
   return <div className="todo">{todo.text}</div>
 }
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add todo..."
+        onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -22,10 +44,16 @@ function App() {
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isComplete: false}];
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => <Todo key={index} index={index} todo={todo} />)}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
