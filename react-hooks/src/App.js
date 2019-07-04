c1ebeff06d1c8b6912aa607e74d8dd6111d2b0ef
Bilger Yahov <byahov@SOF-MBP-026.local>
2019-07-04 09:57:55 +0300

    Initial to-do items & styling.

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
+function Todo({ todo, index }) {
+  return <div className="todo">{todo.text}</div>
+}
+
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Todo 1',
+      isComplete: false
+    },
+    {
+      text: 'Todo 2',
+      isComplete: false
+    },
+    {
+      text: 'Todo 3',
+      isComplete: false
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => <Todo key={index} index={index} todo={todo} />)}
+      </div>
     </div>
   );
 }
